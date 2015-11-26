97d0eb6b6c6eee6945ced41af0fb5ae6b4c7b6a7
trollepierre <trolle.pierre@gmail.com>
2015-11-26 11:33:37 +0100

    Traitement Interface de résolution de problème rectifiée

--- traitement.php
+++ traitement.php
@@ -12,6 +12,7 @@ fputs($monfichier,"\r\n".date("Y-m-d H:i:s")."\r\n");
 //On va vérifier si le jeton est présent dans la session et dans le formulaire
 if((isset($_SESSION['token']) && isset($_SESSION['token_time']) && isset($_POST['token'])))
 {    //Si le jeton de la session correspond à celui du formulaire
+    fputs($monfichier,"\r\n"."je suis là"."\r\n");
     if($_SESSION['token'] == $_POST['token'])
     {
         fputs($monfichier,"SESSION OK"."\r\n");                 echo('SESSION OK'); echo('<br>');
@@ -39,34 +40,33 @@ if((isset($_SESSION['token']) && isset($_SESSION['token_time']) && isset($_POST[
                 $req-> execute(array(  'name_task' => $name_task, 'id_category' => $id_category, 'id_owner' => $id_owner, 'dl' => $dl, 'hl' => $hl, 'prior' => $prior, 'av' => $av, 'ap' => $ap  ));
 
                 fputs($monfichier,'REQUETE EXECUTEE'."\r\n");   echo('REQUETE EXECUTEE');
-                }else{
-                    echo('Problème de HTTP_REFERER');
-                    fputs($monfichier,'Problème de HTTP_REFERER'."\r\n");
-                }
             }else{
-            //header('Location: index.php?bug=OK4');   
-                fputs($monfichier,'Jeton trop vieux - 15 minutes maximum'."\r\n"); echo('Jeton trop vieux - 15 minutes maximum');
+                echo('Problème de HTTP_REFERER');
+                fputs($monfichier,'Problème de HTTP_REFERER'."\r\n");
             }
-            //header('Location: index.php?PAS_DE_BUG');   
-            // echo("D'où sortez-vous ce jeton et/ou comment avez-vous pu changer votre session !");
         }else{
-        
-            if(!(isset($_SESSION['token']))){
-                header('Location: index.php?bug=SESSION_TOKEN');  
-                fputs($monfichier,'Token de session non défini'."\r\n"); echo('Token de session non défini');
-            } 
-            
-            if(!(isset($_SESSION['token_time']))) {
-                header('Location: index.php?bug=SESSION_TOKEN_TIME');
-                fputs($monfichier,'Temps du Token de session non défini'."\r\n"); echo('Temps du Token de session non défini');
-            }
-            
-            if (!(isset($_POST['token']))){
-                header('Location: index.php?bug=TOKEN');
-                fputs($monfichier,'Pas de token envoyé en hidden dans le formulaire, c est pas malin'."\r\n"); echo('Pas de token envoyé en hidden dans le formulaire, c est pas malin');
-            }
+            //header('Location: index.php?bug=OK4');   
+            fputs($monfichier,'Jeton trop vieux - 15 minutes maximum'."\r\n"); echo('Jeton trop vieux - 15 minutes maximum');
         }
+        //header('Location: index.php?PAS_DE_BUG');   
+        // echo("D'où sortez-vous ce jeton et/ou comment avez-vous pu changer votre session !");
+    }else{
+        fputs($monfichier,'Jeton de session différent du jeton donné'."\r\n"); echo('Jeton de session différent du jeton donné');
     }
+}else{
+        if(!(isset($_SESSION['token']))){
+            header('Location: index.php?bug=SESSION_TOKEN');  
+            fputs($monfichier,'Token de session non défini'."\r\n"); echo('Token de session non défini');
+        }             
+        if(!(isset($_SESSION['token_time']))) {
+            header('Location: index.php?bug=SESSION_TOKEN_TIME');
+            fputs($monfichier,'Temps du Token de session non défini'."\r\n"); echo('Temps du Token de session non défini');
+        }            
+        if (!(isset($_POST['token']))){
+            header('Location: index.php?bug=TOKEN');
+            fputs($monfichier,'Pas de token envoyé en hidden dans le formulaire, c est pas malin'."\r\n"); echo('Pas de token envoyé en hidden dans le formulaire, c est pas malin');
+        }
+}
 header('Location: index.php?message=TASKisADDED');
 fputs($monfichier,"Fin programme"."\r\n");                  echo('<br> C EST GAGNE !<br>');
 fclose($monfichier);
